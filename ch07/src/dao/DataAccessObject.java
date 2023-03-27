28a3773536515439a1f1f0dc1516a8b96b524f06
rnalal <youngk8251@naver.com>
2023-03-27 15:11:56 +0900

    add to java

--- ch07/src/dao/DataAccessObject.java
+++ ch07/src/dao/DataAccessObject.java
@@ -1,4 +1,4 @@
-package dao;
+ package dao;
 
 public interface DataAccessObject {
 	public void select();
--- ch07/src/sec5/Java6.java
+++ ch07/src/sec5/Java6.java
@@ -1,13 +1,16 @@
-package sec4;
+package sec5;
 
-public class j1 {
-	public static void main(String[] args){
-		for(int x=1; x<=10; x++){
+public class Java6 {
+
+	public static void main(String[] args) {
+		for(int  x=1; x<=10; x++){
 			for(int y=1; y<=10; y++){
 				if( (4*x + 5*y) == 60){
 					System.out.println("("+x+","+y+")");
 				}
 			}
 		}
+
 	}
+
 }
